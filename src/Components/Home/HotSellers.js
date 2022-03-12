689519ccffb289a8c83d88b693eebe810cc9fea6
shahriarsajeeb <sajeebshahriar665@gmail.com>
2022-03-12 05:55:37 -0800

    new changes

--- src/Components/Home/HotSellers.js
+++ src/Components/Home/HotSellers.js
@@ -11,18 +11,13 @@ export default function HotSellers() {
             <div className="heading">
               <h2 style={{fontSize:'16px'}}>Hot Sellers</h2>
             </div>
-            <div className="viewmore">
-              <a href="#" className="btn bg-blue">
-                View All
-              </a>
-            </div>
           </div>
         </div>
       </div>
       <div className="bg-blue" style={{background:'#f7f7f7'}}>
         <div className="container-fluid">
           <div className="row">
-            <div className="product-layout  product-grid  col-lg-2 col-md-3 col-sm-6 col-xs-12">
+            <div className="product-layout  product-grid  col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="item">
                 <div className="product-thumb">
                   {/* <div className="sale-tag">
@@ -144,7 +139,7 @@ export default function HotSellers() {
                 </div>
               </div>
             </div>
-            <div className="product-layout  product-grid  col-lg-2 col-md-3 col-sm-6 col-xs-12">
+            <div className="product-layout  product-grid  col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="item">
                 <div className="product-thumb">
                   {/* <div className="sale-tag">
@@ -266,7 +261,7 @@ export default function HotSellers() {
                 </div>
               </div>
             </div>
-            <div className="product-layout  product-grid  col-lg-2 col-md-3 col-sm-6 col-xs-12">
+            <div className="product-layout  product-grid  col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="item">
                 <div className="product-thumb">
                   {/* <div className="sale-tag">
@@ -388,7 +383,7 @@ export default function HotSellers() {
                 </div>
               </div>
             </div>
-            <div className="product-layout  product-grid  col-lg-2 col-md-3 col-sm-6 col-xs-12">
+            <div className="product-layout  product-grid  col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="item">
                 <div className="product-thumb">
                   {/* <div className="sale-tag">
@@ -510,251 +505,20 @@ export default function HotSellers() {
                 </div>
               </div>
             </div>
-            <div className="product-layout  product-grid  col-lg-2 col-md-3 col-sm-6 col-xs-12">
-              <div className="item">
-                <div className="product-thumb">
-                  {/* <div className="sale-tag">
-                    <div className="btn bg-blue">SALE</div>
-                    <div className="btn percent bg-yellow">20% OFF</div>
-                  </div> */}
-                  <div className="image product-imageblock">
-                  <div className="btn new bg-blue">SALE</div>
-                    <div className="btn percent bg-yellow">20% OFF</div>
-                    {" "}
-                    <a href="product.html">
-                      <img
-                        src="image/product/toy-4.png"
-                        alt="iPod Classic"
-                        title="iPod Classic"
-                        className="img-responsive"
-                      />
-                    </a>
-                    {/* <ul className="button-group">
-                      <li>
-                        <button
-                          type="button"
-                          className="addtocart-btn bg-green"
-                          title="Add to Cart"
-                        >
-                          {" "}
-                          <i className="fa fa-shopping-cart"></i>{" "}
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                          type="button"
-                          className="wishlist bg-purple"
-                          data-toggle="tooltip"
-                          data-placement="top"
-                          title="Add to Wish List"
-                        >
-                          <i className="fa fa-heart-o"></i>
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                          type="button"
-                          className="quick-view bg-blue"
-                          data-toggle="tooltip"
-                          data-placement="top"
-                          title="Quick View"
-                        >
-                          <i className="fa fa-eye"></i>
-                        </button>
-                      </li>
-                    </ul> */}
-                  </div>
-                  <div className="caption product-detail">
-                    <h4 className="product-name">
-                      <a href="#" title="Casual Shirt With Ruffle Hem">
-                        High Speed Magic Toy
-                      </a>
-                    </h4>
-                    <div className="rating">
-                      {" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                      </span>{" "}
-                    </div>
-                    <p className="price product-price">$122.00</p>
-                    <div className="btn-shooping-cart">
-                      
-                      <button
-                            type="button"
-                            className="quick-view bg-blue"
-                            data-toggle="tooltip"
-                            data-placement="top"
-                            title="Quick View"
-                          >
-                            <i className="fa fa-eye"></i>
-                          </button>
-                      
-                       
-                          <button
-                            type="button"
-                            className="wishlist bg-purple"
-                            data-toggle="tooltip"
-                            data-placement="top"
-                            title="Add to Wish List"
-                          >
-                            <i className="fa fa-heart-o"></i>
-                          </button>
-                     
-                   
-                      
-                        
-                          <button
-                            type="button"
-                            className="addtocart-btn bg-green"
-                            title="Add to Cart"
-                          >
-                            {" "}
-                            <i className="fa fa-shopping-cart"></i>{" "}
-                          </button>
-                      </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="product-layout  product-grid  col-lg-2 col-md-3 col-sm-6 col-xs-12">
-              <div className="item">
-                <div className="product-thumb">
-                  {/* <div className="sale-tag">
-                    <div className="btn bg-blue">SALE</div>
-                    <div className="btn percent bg-yellow">20% OFF</div>
-                  </div> */}
-                  <div className="image product-imageblock">
-                  <div className="btn new bg-blue">SALE</div>
-                    <div className="btn percent bg-yellow">20% OFF</div>
-                    {" "}
-                    <a href="product.html">
-                      <img
-                        src="image/product/toy-4.png"
-                        alt="iPod Classic"
-                        title="iPod Classic"
-                        className="img-responsive"
-                      />
-                    </a>
-                    {/* <ul className="button-group">
-                      <li>
-                        <button
-                          type="button"
-                          className="addtocart-btn bg-green"
-                          title="Add to Cart"
-                        >
-                          {" "}
-                          <i className="fa fa-shopping-cart"></i>{" "}
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                          type="button"
-                          className="wishlist bg-purple"
-                          data-toggle="tooltip"
-                          data-placement="top"
-                          title="Add to Wish List"
-                        >
-                          <i className="fa fa-heart-o"></i>
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                          type="button"
-                          className="quick-view bg-blue"
-                          data-toggle="tooltip"
-                          data-placement="top"
-                          title="Quick View"
-                        >
-                          <i className="fa fa-eye"></i>
-                        </button>
-                      </li>
-                    </ul> */}
-                  </div>
-                  <div className="caption product-detail">
-                    <h4 className="product-name">
-                      <a href="#" title="Casual Shirt With Ruffle Hem">
-                        High Speed Magic Toy
-                      </a>
-                    </h4>
-                    <div className="rating">
-                      {" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                      </span>{" "}
-                    </div>
-                    <p className="price product-price">$122.00</p>
-                    <div className="btn-shooping-cart">
-                      
-                      <button
-                            type="button"
-                            className="quick-view bg-blue"
-                            data-toggle="tooltip"
-                            data-placement="top"
-                            title="Quick View"
-                          >
-                            <i className="fa fa-eye"></i>
-                          </button>
-                      
-                       
-                          <button
-                            type="button"
-                            className="wishlist bg-purple"
-                            data-toggle="tooltip"
-                            data-placement="top"
-                            title="Add to Wish List"
-                          >
-                            <i className="fa fa-heart-o"></i>
-                          </button>
-                     
-                   
-                      
-                        
-                          <button
-                            type="button"
-                            className="addtocart-btn bg-green"
-                            title="Add to Cart"
-                          >
-                            {" "}
-                            <i className="fa fa-shopping-cart"></i>{" "}
-                          </button>
-                      </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            
           </div>
+
+               
+          <div className="viewmore">
+              <a href="#" className="btn" style={{
+                backgroundColor:"#E20025",
+              }}>
+                View All
+              </a>
+            </div>
+          
+
+
         </div>
       </div>
     </section>
--- src/Components/Home/RecentlyViewed.js
+++ src/Components/Home/RecentlyViewed.js
@@ -14,11 +14,7 @@ export default function RecentlyViewed() {
                 <br /> Viewed Products
               </h2>
             </div>
-            <div className="viewmore">
-              <a href="#" className="btn bg-yellow">
-                View All
-              </a>
-            </div>
+           
           </div>
         </div>
       </div>
@@ -26,7 +22,7 @@ export default function RecentlyViewed() {
         <div className="container-fluid">
           <div className="row">
           
-            <div className="product-layout  product-grid  col-lg-2 col-md-3 col-sm-6 col-xs-12">
+            <div className="product-layout  product-grid  col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="item">
                 <div className="product-thumb">
                   {/* <div className="sale-tag">
@@ -149,7 +145,7 @@ export default function RecentlyViewed() {
                 </div>
               </div>
             </div>
-            <div className="product-layout  product-grid  col-lg-2 col-md-3 col-sm-6 col-xs-12">
+            <div className="product-layout  product-grid  col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="item">
                 <div className="product-thumb">
                   {/* <div className="sale-tag">
@@ -271,7 +267,7 @@ export default function RecentlyViewed() {
                 </div>
               </div>
             </div>
-            <div className="product-layout  product-grid  col-lg-2 col-md-3 col-sm-6 col-xs-12">
+            <div className="product-layout  product-grid  col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="item">
                 <div className="product-thumb">
                   {/* <div className="sale-tag">
@@ -393,251 +389,8 @@ export default function RecentlyViewed() {
                 </div>
               </div>
             </div>
-            <div className="product-layout  product-grid  col-lg-2 col-md-3 col-sm-6 col-xs-12">
-              <div className="item">
-                <div className="product-thumb">
-                  {/* <div className="sale-tag">
-                    <div className="btn bg-blue">SALE</div>
-                    <div className="btn percent bg-yellow">20% OFF</div>
-                  </div> */}
-                  <div className="image product-imageblock">
-                  <div className="btn new bg-blue">SALE</div>
-                    <div className="btn percent bg-yellow">20% OFF</div>
-                    {" "}
-                    <a href="product.html">
-                      <img
-                        src="image/product/toy-4.png"
-                        alt="iPod Classic"
-                        title="iPod Classic"
-                        className="img-responsive"
-                      />
-                    </a>
-                    {/* <ul className="button-group">
-                      <li>
-                        <button
-                          type="button"
-                          className="addtocart-btn bg-green"
-                          title="Add to Cart"
-                        >
-                          {" "}
-                          <i className="fa fa-shopping-cart"></i>{" "}
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                          type="button"
-                          className="wishlist bg-purple"
-                          data-toggle="tooltip"
-                          data-placement="top"
-                          title="Add to Wish List"
-                        >
-                          <i className="fa fa-heart-o"></i>
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                          type="button"
-                          className="quick-view bg-blue"
-                          data-toggle="tooltip"
-                          data-placement="top"
-                          title="Quick View"
-                        >
-                          <i className="fa fa-eye"></i>
-                        </button>
-                      </li>
-                    </ul> */}
-                  </div>
-                  <div className="caption product-detail">
-                    <h4 className="product-name">
-                      <a href="#" title="Casual Shirt With Ruffle Hem">
-                        High Speed Magic Toy
-                      </a>
-                    </h4>
-                    <div className="rating">
-                      {" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                      </span>{" "}
-                    </div>
-                    <p className="price product-price">$122.00</p>
-                    <div className="btn-shooping-cart">
-                      
-                      <button
-                            type="button"
-                            className="quick-view bg-blue"
-                            data-toggle="tooltip"
-                            data-placement="top"
-                            title="Quick View"
-                          >
-                            <i className="fa fa-eye"></i>
-                          </button>
-                      
-                       
-                          <button
-                            type="button"
-                            className="wishlist bg-purple"
-                            data-toggle="tooltip"
-                            data-placement="top"
-                            title="Add to Wish List"
-                          >
-                            <i className="fa fa-heart-o"></i>
-                          </button>
-                     
-                   
-                      
-                        
-                          <button
-                            type="button"
-                            className="addtocart-btn bg-green"
-                            title="Add to Cart"
-                          >
-                            {" "}
-                            <i className="fa fa-shopping-cart"></i>{" "}
-                          </button>
-                      </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="product-layout  product-grid  col-lg-2 col-md-3 col-sm-6 col-xs-12">
-              <div className="item">
-                <div className="product-thumb">
-                  {/* <div className="sale-tag">
-                    <div className="btn bg-blue">SALE</div>
-                    <div className="btn percent bg-yellow">20% OFF</div>
-                  </div> */}
-                  <div className="image product-imageblock">
-                  <div className="btn new bg-blue">SALE</div>
-                    <div className="btn percent bg-yellow">20% OFF</div>
-                    {" "}
-                    <a href="product.html">
-                      <img
-                        src="image/product/toy-4.png"
-                        alt="iPod Classic"
-                        title="iPod Classic"
-                        className="img-responsive"
-                      />
-                    </a>
-                    {/* <ul className="button-group">
-                      <li>
-                        <button
-                          type="button"
-                          className="addtocart-btn bg-green"
-                          title="Add to Cart"
-                        >
-                          {" "}
-                          <i className="fa fa-shopping-cart"></i>{" "}
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                          type="button"
-                          className="wishlist bg-purple"
-                          data-toggle="tooltip"
-                          data-placement="top"
-                          title="Add to Wish List"
-                        >
-                          <i className="fa fa-heart-o"></i>
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                          type="button"
-                          className="quick-view bg-blue"
-                          data-toggle="tooltip"
-                          data-placement="top"
-                          title="Quick View"
-                        >
-                          <i className="fa fa-eye"></i>
-                        </button>
-                      </li>
-                    </ul> */}
-                  </div>
-                  <div className="caption product-detail">
-                    <h4 className="product-name">
-                      <a href="#" title="Casual Shirt With Ruffle Hem">
-                        High Speed Magic Toy
-                      </a>
-                    </h4>
-                    <div className="rating">
-                      {" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                        <i className="fa fa-star fa-stack-2x"></i>
-                      </span>{" "}
-                      <span className="fa fa-stack">
-                        <i className="fa fa-star-o fa-stack-2x"></i>
-                      </span>{" "}
-                    </div>
-                    <p className="price product-price">$122.00</p>
-                    <div className="btn-shooping-cart">
-                      
-                      <button
-                            type="button"
-                            className="quick-view bg-blue"
-                            data-toggle="tooltip"
-                            data-placement="top"
-                            title="Quick View"
-                          >
-                            <i className="fa fa-eye"></i>
-                          </button>
-                      
-                       
-                          <button
-                            type="button"
-                            className="wishlist bg-purple"
-                            data-toggle="tooltip"
-                            data-placement="top"
-                            title="Add to Wish List"
-                          >
-                            <i className="fa fa-heart-o"></i>
-                          </button>
-                     
-                   
-                      
-                        
-                          <button
-                            type="button"
-                            className="addtocart-btn bg-green"
-                            title="Add to Cart"
-                          >
-                            {" "}
-                            <i className="fa fa-shopping-cart"></i>{" "}
-                          </button>
-                      </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="product-layout  product-grid  col-lg-2 col-md-3 col-sm-6 col-xs-12">
+          
+            <div className="product-layout  product-grid  col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="item">
                 <div className="product-thumb">
                   {/* <div className="sale-tag">
@@ -760,6 +513,18 @@ export default function RecentlyViewed() {
               </div>
             </div>
           </div>
+                
+
+          <div className="viewmore">
+          <a href="#" className="btn" style={{
+                backgroundColor:"#E20025",
+                color:"#fff"
+              }}>
+                View All
+              </a>
+            </div>
+
+
         </div>
       </div>
     </section>
